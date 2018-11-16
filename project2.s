534643f16ba9ec5dd65b0ea66ddeb70cd6016328
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-11-16 18:44:41 -0500

    Made a syscall for user input and loaded byte space to address

--- project2.s
+++ project2.s
@@ -1,5 +1,10 @@
 .data
-user_input: .space 10000
+user_input: .space 11000
 input_value_empty: .asciiz "Input is empty."
 input_value_invalid: .asciiz "Invalid base-27 number."
 input_too_long: .asciiz "Input is too long."
+
+.text
+main:
+li $v0, 8                                   #  syscall code to get user input
+la $a0, user_input                          #  loading byte space into address

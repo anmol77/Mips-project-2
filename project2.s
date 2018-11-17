9fafcfb49d3af699f10e2e76cc8884ae479d7dd2
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-11-16 19:11:18 -0500

    Kept the copy of a string in other register and moved it to prevent its alternation

--- project2.s
+++ project2.s
@@ -8,3 +8,7 @@ input_too_long: .asciiz "Input is too long."
 main:
 li $v0, 8                                   #  syscall code to get user input
 la $a0, user_input                          #  loading byte space into address
+li $a1, 11000                               #  allocating byte space for input string
+syscall
+move $t8, $a0                               #  keeping the copy of string in other register
+move $t0, $a0                               #  move string to t0
